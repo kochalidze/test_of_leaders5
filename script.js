6b54cc9eb256529d4e1a33da1815c8f9d7a4d74d
kochalidze <kochalidzegiga11@gmail.com>
2025-04-13 19:29:41 +0400

    second commit

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
-// IPA: https://fakestoreapi.com/products
+const productsContainer = document.getElementById("products");
+const cartContainer = document.getElementById("cart");
+const totalElement = document.getElementById("total");
 
-const products = document.getElementById('products');
-const cartCont = document.getElementById('cart');
-const totalEle = document.getElementById('total');
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-fetch('https://fakestoreapi.com/products')
-	.then((res) => res.json())
-	.then((p) => {
-		p.forEach(el => {
-			const card = document.createElement('div');
-			card.className = 'card'
-			card.innerHTML = `
-			<img src='${el.image}' />
-			<h4>${el.title}</h4>
-			<p>$ ${el.price}</p>
-			<button onclick='addToCart()'>Add to cart</button>
-			`;
-			products.appendChild(card);
-		});
-	})
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((products) => {
+    products.forEach((product) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+        <img src="${product.image}" alt="${product.title}" />
+        <h3>${product.title}</h3>
+        <p>$${product.price}</p>
+        <button onclick='addToCart(${JSON.stringify(product)})'>Add to Cart</button>
+      `;
+      productsContainer.appendChild(card);
+    });
+  });
 
 function addToCart(product) {
-	cart.push(product);
-	localStorage.setItem('cart', JSON.stringify(cart));
-	renderCart();
+  cart.push(product);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
 }
+
 function renderCart() {
-	cartCont.innerHTML = '';
-	let total = 0;
-	cart.forEach((i, index) => {
-		total += i.price;
-		const div = document.createElement('div');
-		div.innerHTML = `
-		${i.title}-$${i.price.toFixed(2)}
-		<button onclick='removeFromCart(${index})'>Remove</button>
-		`;
-		cartCont.appendChild(div);
-	});
-	totalEle.innerHTML = `Total: $${total.toFixed(2)}`
+  cartContainer.innerHTML = "";
+  let total = 0;
+
+  cart.forEach((item, index) => {
+    total += item.price;
+    const div = document.createElement("div");
+    div.innerHTML = `
+      ${item.title} - $${item.price.toFixed(2)}
+      <button onclick="removeFromCart(${index})">Remove</button>
+    `;
+    cartContainer.appendChild(div);
+  });
+
+  totalElement.innerText = `Total: $${total.toFixed(2)}`;
 }
 
 function removeFromCart(index) {
-	cart.splice(index, 1);
-	localStorage.setItem('cart', JSON.stringify(cart));
-	renderCart();
+  cart.splice(index, 1);
+  localStorage.setItem("cart", JSON.stringify(cart));
+  renderCart();
 }
 
-function clearCart(){
-	cart = [];
-	localStorage.removeItem('cart');
-	renderCart()
+function clearCart() {
+  cart = [];
+  localStorage.removeItem("cart");
+  renderCart();
 }
 
+// Initial render
 renderCart();
